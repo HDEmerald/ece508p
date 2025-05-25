245105b01585231381c8747158bdcae05d7a7fa5
hdemerald <tripled5.hunter5@gmail.com>
2025-05-24 21:51:19 -0700

    added more framework for layer mgmt. (e.g. layer name and color)

--- src/gui_api.py
+++ src/gui_api.py
@@ -1,9 +1,29 @@
 import tkinter as tk
+from tkinter import colorchooser, simpledialog, messagebox
+import random as r
+
+class Layer:
+    def __init__(self, name: str, bg_color: str="#FFFFFF", layer_hidden: bool=False):
+        self.name = name
+        self.bg_color = bg_color
+        self.text_color = "black" if not layer_hidden else "lightgrey"
+        self.layer_hidden = layer_hidden
+
+    def toggle_hide(self):
+        self.text_color = "black" if self.layer_hidden else "lightgrey"
+        self.layer_hidden = not self.layer_hidden
+
+    def text_colors(self):
+        return (self.text_color,self.bg_color)
+    
+    def is_hidden(self):
+        return self.layer_hidden
 
 class LayersBar:
     def __init__(self, root):
+        self.layers = {}
         self.curr_idx = None
-        self.layer_hidden = {}
+        self.layer_menu_idx = -1
 
         self.root = root
         self.frame = tk.Frame(root)
@@ -12,61 +32,173 @@ class LayersBar:
         self.header = tk.Frame(self.frame)
         self.label = tk.Label(self.header,text="Layers",justify='left')
         self.label.pack(side=tk.LEFT)
-        self.del_button = tk.Button(self.header,text="-",width=1,height=1,command=self.delete)
+        self.del_button = tk.Button(self.header,text="-",width=1,height=1,
+                                    command=self.delete_layer)
         self.del_button.pack(side=tk.LEFT,anchor=tk.W)
-        self.add_button = tk.Button(self.header,text="+",width=1,height=1,command=self.add)
+        self.add_button = tk.Button(self.header,text="+",width=1,height=1,
+                                    command=self.add_layer)
         self.add_button.pack(side=tk.LEFT,anchor=tk.W)
-        self.hide_button = tk.Button(self.header,text="H",width=1,height=1,command=self.hide)
+        self.hide_button = tk.Button(self.header,text="H",width=1,height=1,
+                                     command=self.hide_layer)
         self.hide_button.pack(side=tk.LEFT,anchor=tk.W)
         self.header.pack()
 
-        # TODO Add checkboxs for each layer
         # list body/scroll bar
         self.scroll = tk.Scrollbar(self.frame,orient=tk.VERTICAL)
-        self.list = tk.Listbox(self.frame,yscrollcommand=self.scroll.set,selectmode=tk.SINGLE)
-        self.list.bind("<Button-1>",self.sel_item)
-        self.list.bind("<B1-Motion>",self.drag_item)
+        self.list = tk.Listbox(self.frame,yscrollcommand=self.scroll.set,selectmode=tk.SINGLE,
+                               activestyle='underline')
+        self.list.bind("<Button-1>",self.sel_layer)
+        self.list.bind("<B1-Motion>",self.drag_layer)
         self.scroll.config(command=self.list.yview)
         self.scroll.pack(side=tk.RIGHT,fill=tk.Y)
         self.list.pack(side=tk.RIGHT,fill=tk.Y)
 
+        # right-click layer menu
+        self.layer_menu = tk.Menu(self.frame,tearoff=False)
+        self.layer_menu.add_command(label="Change Color",command=self.change_layer_color)
+        self.layer_menu.add_command(label="Rename Layer",command=self.change_layer_name)
+        self.list.bind("<Button-3>",self.show_layer_menu)
+
         self.frame.pack(side=tk.RIGHT,fill=tk.Y)
 
-    def sel_item(self, event):
+    def sel_layer(self, event):
         self.curr_idx = self.list.nearest(event.y)
 
-    def drag_item(self, event):
+    def drag_layer(self, event):
         idx = self.list.nearest(event.y)
+        layer = self.list.get(idx)
+        text_colors = self.layers[layer].text_colors()
         if idx < self.curr_idx:
-            x = self.list.get(idx)
             self.list.delete(idx)
-            self.list.insert(idx+1, x)
-            self.curr_idx = idx
+            self.list.insert(idx+1,layer)
+            self.list.itemconfig(idx+1,fg=text_colors[0],bg=text_colors[1])
         elif idx > self.curr_idx:
-            x = self.list.get(idx)
             self.list.delete(idx)
-            self.list.insert(idx-1, x)
-            self.curr_idx = idx
+            self.list.insert(idx-1,layer)
+            self.list.itemconfig(idx-1,fg=text_colors[0],bg=text_colors[1])
 
-    def delete(self):
-        idx = self.list.index(tk.ANCHOR)
-        del self.layer_hidden[self.list.get(idx)]
-        self.list.delete(tk.ANCHOR)
-    
-    def add(self):
+        self.curr_idx = idx
+
+    def show_layer_menu(self, event):
+        self.layer_menu_idx = self.list.nearest(event.y)
+        if self.layer_menu_idx != -1:
+            self.layer_menu.tk_popup(event.x_root,event.y_root)
+            return True
+        else:
+            return False
+
+    def change_layer_color(self):
+        idx = self.layer_menu_idx
+        name = self.list.get(idx)
+        bg_color = colorchooser.askcolor(title=f"Select Color for {name}")
+        self.list.itemconfig(self.layer_menu_idx,bg=bg_color[1])
+        return True
+
+    def change_layer_name(self):
+        idx = self.layer_menu_idx
+        old_name = self.list.get(idx)
+        bg_color = self.list.itemcget(idx,"background")
+        hidden_status = self.layers[old_name].is_hidden()
+
+        new_name = simpledialog.askstring("Input", f"New name for {old_name}")
+
+        if new_name in self.list.get(0,tk.END) and new_name != old_name:
+            messagebox.showerror("Layer Rename Error","Error: Layer name already exists!")
+            return False
+        elif new_name is not None:
+            # delete old entry
+            del self.layers[old_name]
+            self.list.delete(idx)
+            # add renamed entry
+            if self.add_layer(idx,new_name,bg_color,hidden_status):
+                return True
+        else:
+            # simply no change to layer name
+            return True
+
+    def rand_color(self):
+        # create a random color hexcode (6-hexadecimal digits)
+        color = hex(r.randint(0,0xFFFFFF)).replace("0x","")
+        while len(color) < 6:
+            color = "0" + color
+        return "#" + color
+
+    def delete_layer(self, idx: int=-1, del_cursor: bool=True):
+        # if a layer is selected, delete it
         end_idx = self.list.index(tk.END)
-        self.list.insert(tk.END, f"Layer{end_idx}")
-        self.layer_hidden.update({self.list.get(tk.END) : False})
+        if del_cursor:
+            try: idx = self.list.curselection()[0]
+            except: idx = None
+
+        if idx == None:
+            # nothing to delete... do nothing
+            return True
+        
+        name = self.list.get(idx)
+        if idx >= 0 and idx <= end_idx and name != "":
+            del self.layers[name]
+            self.list.delete(idx)
+        elif idx < 0 or idx > end_idx:
+            print(f"Error: Invalid idx in LayersBar.delete_layer()... idx = {idx} last index = {end_idx}")
+            return False
+        else:
+            print(f"Error: Empty name in LayersBar.delete_layer()...")
+            return False
+
+    
+    def add_layer(self, idx=-1, name: str="", bg_color: str="", hidden_status: bool=False):
+        # make sure layer name is not taken
+        end_idx = self.list.index(tk.END)
+        if name == "":
+            temp_idx = 0
+            name = f"Layer{temp_idx}"
+            while name in self.list.get(0,tk.END):
+                temp_idx += 1
+                name = f"Layer{temp_idx}"
+
+        # assign a random color if none given
+        if bg_color == "":
+            bg_color = self.rand_color()
+
+        # put layer in the right place
+        if idx == -1:
+            idx = end_idx
+            self.list.insert(tk.END, name)
+            self.list.itemconfig(tk.END,bg=bg_color)
+            new_layer = Layer(name,bg_color,hidden_status)
+            self.layers.update({name : new_layer})
+        elif idx >= 0 and idx <= end_idx:
+            self.list.insert(idx, name)
+            for i,j in enumerate(self.list.get(0,tk.END)): 
+                if j == name: 
+                    idx = i
+                    break
+            self.list.itemconfig(idx,bg=bg_color)
+            new_layer = Layer(name,bg_color,hidden_status)
+            self.layers.update({name : new_layer})
+        else:
+            print(f"Error: Invalid idx in LayersBar.add_layer()... idx = {idx}")
+            return False
+
+        # make sure layer hidden status aligns with text color
+        if hidden_status == True:
+            self.list.itemconfig(idx,fg="lightgrey")
+
+        return True
 
     # TODO Add layer hiding functionality
-    def hide(self):
-        layer = self.list.get(tk.ANCHOR)
-        if not self.layer_hidden[layer]:
-            self.layer_hidden[layer] = True
-            self.list.itemconfig(tk.ANCHOR,foreground="lightgrey")
-        else:
-            self.layer_hidden[layer] = False
-            self.list.itemconfig(tk.ANCHOR,foreground="black")
+    def hide_layer(self):
+        # if a layer is not selected, exit with no changes
+        idx = self.list.curselection()
+        if not idx:
+            return False
+        
+        # toggle layer to be hidden/shown
+        layer = self.list.get(idx)
+        self.layers[layer].toggle_hide()
+        text_color = self.layers[layer].text_colors()[0]
+        self.list.itemconfig(idx,fg=text_color)
+        return True
 
 class MenuBar:
     def __init__(self, root):
